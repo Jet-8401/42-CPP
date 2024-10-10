26b164885b84805bbebc5a8c342046c245ec7c6b
Julien Lopez <jullopez@c1r2s3.42luxembourg.lu>
2024-10-10 13:52:22 +0200

    done ex02

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -1,6 +1,74 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+# include <iostream>
+# include <exception>
+typedef unsigned int uint;
 
+namespace {
+
+	template <typename T>
+	class Array {
+		private:
+			uint	_length;
+			T		*_intern_arr;
+
+		public:
+			class OutOfBoundException : public std::exception {
+				const char* what() const throw() {
+					return "Accessing index is out of bound";
+				}
+			};
+
+			Array(void): _length(0) {
+				this->_intern_arr = new T[0];
+				return ;
+			}
+
+			Array(uint n): _length(n) {
+				this->_intern_arr = new T[n];
+				for (uint i = 0; i < this->_length; i++)
+					this->_intern_arr[i] = 0;
+				return ;
+			}
+
+			Array(const Array& src): _length(src._length) {
+				this->_intern_arr = new T[this->_length];
+				*this = src;
+				return ;
+			}
+
+			virtual	~Array(void) {
+				delete [] this->_intern_arr;
+				return ;
+			}
+
+			Array&	operator=(const Array& rhs) {
+				if (!this->_intern_arr || !rhs._intern_arr)
+					return (*this);
+				for (uint i = 0; i < this->_length && i < rhs._length; i++)
+					this->_intern_arr[i] = rhs._intern_arr[i];
+				return (*this);
+			}
+
+			T&		operator[](const uint n) const {
+				if (n >= this->_length || !this->_intern_arr)
+					throw OutOfBoundException();
+				return (this->_intern_arr[n]);
+			}
+
+			uint	size(void) const {
+				return (this->_length);
+			}
+	};
+
+	template<typename T>
+	void	printArray(::Array<T>& arr)
+	{
+		for (uint i = 0; i < arr.size(); i++)
+			std::cout << "array[" << i << "]=" << arr[i] << std::endl;
+		return ;
+	}
+}
 
 #endif
--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include "Array.hpp"
 
 int	main(void)
 {
-	int * a = new int();
+	::Array<int>	arr(5);
+	for (uint i = 0; i < arr.size(); i++) {
+		arr[i] = i;
+		std::cout << arr[i] << std::endl;
+	}
 
-	std::cout << *a << std::endl;
+	::Array<int>	arrayCopy(arr);
+	::Array<int>	arrayCopyCopy = arrayCopy;
+	arrayCopyCopy[0] = 398203;
+
+	std::cout << "-- arrayCopy --" << std::endl;
+	::printArray<int>(arrayCopy);
+	std::cout << "-- arayCopyCopy --" << std::endl;
+	::printArray<int>(arrayCopyCopy);
+
+	std::cout << "-- string array --" << std::endl;
+	::Array<char>	string(20);
+	for (uint i = 0; i < string.size(); i++)
+		string[i] = 'a' + i;
+
+	::Array<char>	smallString(10);
+	smallString = string;
+	string[1] = 'k';
+	::printArray<char>(smallString);
+	std::cout << std::endl;
+
+	// smallString[20]; // will throw an exception
+
 	return (0);
 }
